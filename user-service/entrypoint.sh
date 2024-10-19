f80e26bc4a8130a8d229db95df0bc97fce43eee3
BrunoPolaski <polaskibruno03@gmail.com>
2024-10-18 22:13:40 -0300

    feat: docker compose done with services

--- user-service/entrypoint.sh
+++ user-service/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-echo "Script executed from: ${PWD}"
-composer install --ignore-platform-reqs
-php -S 0.0.0.0:8080 -t src src/index.php &
-sleep 5
-php ./src/local-aws-settings.php
+composer install --ignore-platform-reqs --no-scripts
+php -S 0.0.0.0:8081 -t bootstrap bootstrap/app.php
