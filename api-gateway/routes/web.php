81963e934148a8a17bb0896d8ea8d12e34335e6d
BrunoPolaski <polaskibruno03@gmail.com>
2024-11-06 21:54:30 -0300

    feat: adjuste some things inside api gateway

--- api-gateway/routes/web.php
+++ api-gateway/routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
 use Illuminate\Support\Facades\Route;
 use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\Request;
 
 function filterHeaders($headers) {
@@ -14,40 +13,37 @@ function filterHeaders($headers) {
     }, ARRAY_FILTER_USE_KEY);
 }
 
-function decodeJwtToken($token, $secretKey) {
-    try {
-        return JWT::decode($token, new Key($secretKey, 'HS256'));
-    } catch (Exception $e) {
-        return null;
-    }
-}
-
 Route::any(
     '/user/{path}',
     function(Request $request, $path) {
-        $client = new HttpClient([
-            'base_uri' => 'http://localhost:8081'
-        ]);
+        try {
+            $client = new HttpClient([
+                // send to the user service container
+                'base_uri' => 'user-service',
+            ]);
 
-        $resp = $client->request(
-            $request->method(),
-            $path,
-            [
-                'headers' => filterHeaders($request->header()),
-                'query' => $request->query(),
-                'body' => $request->getContent()
-            ]
-        );
-
-        return response(
-            $resp
-                ->getBody()
-                ->getContents(),
-            $resp
-                ->getStatusCode()
-        )->withHeaders(
-            filterHeaders($resp->getHeaders())
-        );
+            $resp = $client->request(
+                $request->method(),
+                $path,
+                [
+                    'headers' => filterHeaders($request->header()),
+                    'query' => $request->query(),
+                    'body' => $request->getContent()
+                ]
+            );
+            
+            return response(
+                $resp
+                    ->getBody()
+                    ->getContents(),
+                $resp
+                    ->getStatusCode()
+            )->withHeaders(
+                filterHeaders($resp->getHeaders())
+            );
+        } catch (GuzzleException $e) {
+            echo $e->getMessage();
+        }
     }
 )->where('path', '.*');
 
