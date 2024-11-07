078a5673bdb0c68825ec306b0f0527850ed7f758
BrunoPolaski <polaskibruno03@gmail.com>
2024-11-06 21:55:16 -0300

    feat: changed docker things

--- notification-service/entrypoint.sh
+++ notification-service/entrypoint.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-# Run migrations
-composer install --no-dev --optimize-autoloader
-php artisan migrate --force
-
-# Start Apache
-exec apache2-foreground
+sleep 10
+composer install --ignore-platform-reqs --no-scripts
+composer dump-autoload --optimize
+php artisan migrate
+php artisan storage:link
+php artisan serve --host=0.0.0.0 --port=8084
