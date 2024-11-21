3ea64b304e7c5c16f3dbdec3d8e9b63abcf9d7d1
Douglas Liebl <douglasliebl@outlook.com>
2024-11-21 11:16:22 -0300

    java gateway fuck yeah

--- api-gateway/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sleep 10
-composer install --ignore-platform-reqs --no-scripts
-composer dump-autoload --optimize
-php artisan migrate
-php artisan storage:link
-php artisan serve --host=0.0.0.0 --port=8080
