9798c89f8512c873c46091369d639ecae82fc2cf
BrunoPolaski <polaskibruno03@gmail.com>
2024-11-21 23:29:53 -0300

    feat: starting front-end

---
+++ front-end/src/layouts/MainLayout.vue
@@ -0,0 +1,77 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+
+          round
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        >
+          <!-- change image on hover-->
+          <q-img :src="!leftDrawerOpen ? 'icons/menu.png' : 'icons/menu-closed.png'" />
+        </q-btn>
+
+        <q-toolbar-title class="text-center">
+          Reserva de restaurante
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      :mini="leftDrawerOpen"
+      show-if-above
+      class="animated"
+      :width="150"
+      :model-value="true"
+    >
+      <q-tabs
+        vertical
+        v-model="tab"
+        class="bg-secondary"
+        active-color="black"
+        indicator-color="black"
+        switch-indicator
+      >
+        <q-tab
+          name="home"
+          icon="img:icons/home.png"
+          :label="leftDrawerOpen ? ' ' : 'Home'"
+          to="/"
+        />
+
+        <q-tab
+          name="reservations"
+          icon="img:icons/booking.png"
+          :label="leftDrawerOpen ? ' ' : 'Reservations'"
+          to="/reservations"
+        />
+
+        <q-tab
+          name="menu"
+          icon="img:icons/restaurant.png"
+          :label="leftDrawerOpen ? ' ' : 'Menu'"
+          to="/menu"
+        />
+      </q-tabs>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const leftDrawerOpen = ref(false);
+const tab = ref('home');
+
+function toggleLeftDrawer () {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
---
+++ front-end/src/pages/ErrorNotFound.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="fullscreen text-white text-center q-pa-md flex flex-center"
+    style="
+      background-image: url('/rota_errada.jpg');
+      background-repeat: no-repeat;
+      background-size: contain;
+      background-position: center;
+      background-color: black;
+      ">
+    <div>
+      <div style="font-size: 30vh">
+        404
+      </div>
+
+      <div class="column text-h2 bg-black medium-border-radius">
+        Rota errada, amigo.
+
+      </div>
+      <q-btn
+      class="q-mt-xl"
+      color="white"
+      text-color="black"
+      unelevated
+      to="/"
+      label="Quero voltaaaaaaaaar"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'ErrorNotFound'
+});
+</script>
