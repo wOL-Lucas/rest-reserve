76f91792953a9bb8afaa4a9130e47845ef5d1d46
Douglas Liebl <douglasliebl@outlook.com>
2024-11-29 09:24:08 -0300

    feat/fix: reservation service added to api-gateway and fixes on php images version

--- reservation-service/entrypoint.sh
+++ reservation-service/entrypoint.sh
@@ -5,4 +5,4 @@ composer install --ignore-platform-reqs --no-scripts
 composer dump-autoload --optimize
 php artisan migrate
 php artisan storage:link
-php artisan serve --host=0.0.0.0 --port=8082
+php artisan serve --host=0.0.0.0 --port=8083
